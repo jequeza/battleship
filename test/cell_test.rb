33e4367d5acb64a7463d157c10e6e66eeb6b2fd5
Jesus Quezada-Guillen <maj3st1@gmail.com>
2020-10-13 12:53:31 -0500

    Refractor code for cell class and test.

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -7,12 +7,10 @@ require './lib/cell'
 class CellTest < MiniTest::Test
 
   def test_it_exists
-    # skip
     cell = Cell.new("B4")
     assert_equal "B4", cell.coordinate
     assert_nil nil, cell.ship
     assert_equal true, cell.empty?
-
     cruiser = Ship.new("Cruiser", 3)
     cell.place_ship(cruiser)
     assert_equal "Cruiser", cell.ship.name
@@ -20,16 +18,13 @@ class CellTest < MiniTest::Test
   end
 
   def test_fired_upon_is_false
-    # skip
     cell = Cell.new("B4")
     cruiser = Ship.new("Cruiser", 3)
     cell.place_ship(cruiser)
-
     assert_equal false, cell.fired_upon?
   end
 
   def test_it_took_fire
-    # skip
     cell = Cell.new("B4")
     cruiser = Ship.new("Cruiser", 3)
     cell.place_ship(cruiser)
@@ -39,23 +34,19 @@ class CellTest < MiniTest::Test
   end
 
   def test_render_method
-    # skip
     cell_1 = Cell.new("B4")
     cell_2 = Cell.new("C3")
     cruiser = Ship.new("Cruiser", 3)
-
     assert_equal ".", cell_1.render
     cell_1.fire_upon
     assert_equal "M", cell_1.render
   end
 
   def test_render_method_Hit
-    # skip
     cell_1 = Cell.new("B4")
     cell_2 = Cell.new("C3")
     cruiser = Ship.new("Cruiser", 3)
     cell_2.place_ship(cruiser)
-
     assert_equal ".", cell_2.render
     cell_2.fire_upon
     assert_equal "H", cell_2.render
@@ -66,7 +57,6 @@ class CellTest < MiniTest::Test
     cell_2 = Cell.new("C3")
     cruiser = Ship.new("Cruiser", 3)
     cell_2.place_ship(cruiser)
-
     assert_equal "S", cell_2.render(true)
     cell_2.fire_upon
     assert_equal "H", cell_2.render
@@ -76,4 +66,5 @@ class CellTest < MiniTest::Test
     assert_equal true, cruiser.sunk?
     assert_equal "X", cell_2.render
   end
+  
 end
