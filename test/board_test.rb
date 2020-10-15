736670ee58566d0a1db49aade1bdd15185767c18
Jesus Quezada-Guillen <maj3st1@gmail.com>
2020-10-15 15:02:03 -0500

    Add test for valid_placement? method.

--- test/board_test.rb
+++ test/board_test.rb
@@ -11,6 +11,9 @@ class BoardTest < MiniTest::Test
     board = Board.new("Player one board")
     assert_instance_of Board, board
     assert_equal 16, board.cells.keys.count
+    assert_equal 16, board.cells.values.count
+    assert_equal "B4", board.cells.keys[7]
+    assert_equal "C3", board.cells.keys[10]
   end
 
   def test_it_can_validate_coordinates
@@ -20,6 +23,24 @@ class BoardTest < MiniTest::Test
     assert_equal false, board.valid_coordinate?("A5")
     assert_equal false, board.valid_coordinate?("E1")
     assert_equal false, board.valid_coordinate?("A22")
+  end
+
+  def test_it_can_validate_placement
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+    assert_equal false, board.valid_placement?(cruiser, ["A1", "A2"])
+    assert_equal false, board.valid_placement?(submarine, ["A2", "A3", "A4"])
+
+    assert_equal false, board.valid_placement?(cruiser, ["A1", "A2", "A4"])
+    assert_equal false, board.valid_placement?(submarine, ["A1", "C1"])
+    assert_equal false, board.valid_placement?(cruiser, ["A3", "A2", "A1"])
+    assert_equal false, board.valid_placement?(submarine, ["C1", "B1"])
+    assert_equal false, board.valid_placement?(cruiser, ["A1", "B2", "C3"])
+    assert_equal false, board.valid_placement?(submarine, ["C2", "D3"])
+
+    assert_equal true, board.valid_placement?(submarine, ["A1", "A2"])
+    assert_equal true, board.valid_placement?(cruiser, ["B1", "C1", "D1"])
   end
 
 
