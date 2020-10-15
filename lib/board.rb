1c0a42319161bbc5d9fcd7bfb4d5942d7c65b654
Jesus Quezada-Guillen <maj3st1@gmail.com>
2020-10-15 16:52:45 -0500

    Add test and methods for render.

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,6 @@
 require './lib/check'
 require './lib/ship'
+require 'pry'
 
 class Board
   attr_reader :name, :cells
@@ -26,19 +27,31 @@ class Board
     }
   end
 
+  def render(location = false)
+    if location == true
+      "\n#{@name}
+        1  2  3  4
+      A #{@cells["A1"].render(true)}  #{@cells["A2"].render(true)}  #{@cells["A3"].render(true)}  #{@cells["A4"].render(true)}
+      B #{@cells["B1"].render(true)}  #{@cells["B2"].render(true)}  #{@cells["B3"].render(true)}  #{@cells["B4"].render(true)}
+      C #{@cells["C1"].render(true)}  #{@cells["C2"].render(true)}  #{@cells["C3"].render(true)}  #{@cells["C4"].render(true)}
+      D #{@cells["D1"].render(true)}  #{@cells["D2"].render(true)}  #{@cells["D3"].render(true)}  #{@cells["D4"].render(true)}
+      "
+    else
+      "\n#{@name}
+        1  2  3  4
+      A #{@cells["A1"].render}  #{@cells["A2"].render}  #{@cells["A3"].render}  #{@cells["A4"].render}
+      B #{@cells["B1"].render}  #{@cells["B2"].render}  #{@cells["B3"].render}  #{@cells["B4"].render}
+      C #{@cells["C1"].render}  #{@cells["C2"].render}  #{@cells["C3"].render}  #{@cells["C4"].render}
+      D #{@cells["D1"].render}  #{@cells["D2"].render}  #{@cells["D3"].render}  #{@cells["D4"].render}
+      "
+    end
+  end
+
   def valid_coordinate?(coordinate)
     if cells.include?(coordinate)
       true
     else
       false
-    end
-  end
-
-  def valid_placement?(ship, coordinates)
-    if ship.length == coordinates.length
-     Check.new(coordinates).valid
-    else
-     false
     end
   end
 
@@ -48,11 +61,18 @@ class Board
     end
   end
 
-  def place(ship_name, coordinates)
-    coordinates.map do |coordinate|
-     @cells[coordinate].place_ship(ship_name) && !overlap?(coordinates)
+  def valid_placement?(ship, coordinates)
+    if ship.length == coordinates.length && !overlap?(coordinates)
+     Check.new(coordinates).valid
+    else
+     false
     end
   end
 
+  def place(ship_name, coordinates)
+    coordinates.map do |coordinate|
+     @cells[coordinate].place_ship(ship_name)
+    end
+  end
 
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -46,9 +46,7 @@ class BoardTest < MiniTest::Test
   def test_it_can_place_ships
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
-
     board.place(cruiser, ["A1", "A2", "A3"])
-
     cell_1 = board.cells["A1"]
     cell_2 = board.cells["A2"]
     cell_3 = board.cells["A3"]
@@ -58,6 +56,23 @@ class BoardTest < MiniTest::Test
     assert cell_3.ship == cell_2.ship
     assert cell_1.ship == cell_2.ship
     assert_instance_of Ship, cell_1.ship
+  end
+
+  def test_it_can_tell_if_ships_overlap
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    board.place(cruiser, ["A1", "A2", "A3"])
+    submarine = Ship.new("Submarine", 2)
+    assert_equal false, board.valid_placement?(submarine, ["A1", "B1"])
+  end
+
+
+  def test_it_can_render_the_board
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    board.place(cruiser, ["A1", "A2", "A3"])
+    board.render
+    board.render(true)
   end
 
 end
