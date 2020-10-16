3962cbd39c74680fc41343240f923b665c620c00
Jesus Quezada-Guillen <maj3st1@gmail.com>
2020-10-15 21:19:50 -0500

    Create computer class skeleton file.

---
+++ lib/computer.rb
@@ -0,0 +1,25 @@
+require './lib/board'
+require 'pry'
+
+class Computer
+  def initialize(argument_1)
+    @argument_1 = argument_1
+  end
+
+  def generate_computer_board
+    # Creates computer board, displays all except for ship placement.
+  end
+
+  def generate_ship_placement
+    # Places computer ships on the board randomly passing all validation
+    #  from array of options
+  end
+
+
+
+  def generate_coordinates_for_turn
+    # generates random coordinates for turn,
+  end
+
+
+end
