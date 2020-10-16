ee577ffb98b1160dafbd99fd11113679069dfc0e
Jesus Quezada-Guillen <maj3st1@gmail.com>
2020-10-15 21:31:24 -0500

    Create player skeleton file.

---
+++ lib/player.rb
@@ -0,0 +1,21 @@
+class Player
+  def initialize(argument_1)
+    @argument_1 = argument_1
+  end
+
+  def generate_player_board
+    # generates player board
+  end
+
+  def place_player_ships
+    # get input from player for placing ships. input gets validated
+    # if pass, then ships are placed (*keep looping if not)
+  end
+
+  def player_damage_phase
+    # gets input from player on what coordinates to fire upon, validates,
+    # stores every previous selection. Updates computer board display
+  end
+
+
+end
