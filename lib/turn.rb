47d6b5d38d958218acdf7c1c68549904973234fd
Jesus Quezada-Guillen <maj3st1@gmail.com>
2020-10-15 21:36:52 -0500

    Create turn class skeleton file.

---
+++ lib/turn.rb
@@ -0,0 +1,20 @@
+class Turn
+  def initialize(argument_1)
+    @argument_1 = argument_1
+  end
+
+  def display_boards
+    # displays boards. player can not see ship placement of computer board
+    # player can see his placement of ships
+  end
+
+  def player_damage
+
+  end
+
+  def computer_damage
+
+  end
+
+
+end
