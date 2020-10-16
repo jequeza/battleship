9f6a49111678f66bf9278b8bd3e7974009733c44
James Fox-Collis <jlfoxcollis@gmail.com>
2020-10-15 20:47:16 -0700

    Player test passing placment of ships.

--- lib/player.rb
+++ lib/player.rb
@@ -1,13 +1,40 @@
+require './lib/board.rb'
+
+
 class Player
-  def initialize(argument_1)
-    @argument_1 = argument_1
+  attr_reader :board
+
+  def initialize
+    @board = Board.new('Player_1:')
+    @possible = ["A1","A2","A3","A4","B1","B2","B3","B4","C1","C2","C3","C4","D1","D2","D3","D4"]
+
   end
 
-  def generate_player_board
-    # generates player board
+
+  def place_player_ship_two(ship = submarine, input)
+    if @board.valid_placement?(ship, input)
+      coordinate = []
+      coordinate << input
+      coordinate.map do |coord|
+        @board.place(ship, coord)
+      end
+    else
+      false
+    end
+    # get input from player for placing ships. input gets validated
+    # if pass, then ships are placed (*keep looping if not)
   end
 
-  def place_player_ships
+  def place_player_ship_three(ship = cruiser, input)
+    if @board.valid_placement?(ship, input)
+      coordinate = []
+      coordinate << input
+      coordinate.map do |coord|
+        @board.place(ship, coord)
+      end
+    else
+      false
+    end
     # get input from player for placing ships. input gets validated
     # if pass, then ships are placed (*keep looping if not)
   end
---
+++ test/player_test.rb
@@ -0,0 +1,45 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+require './lib/player'
+
+class PlayerTest < Minitest::Test
+
+  def test_player_has_a_board
+    skip
+    player_1 = Player.new
+
+    assert_equal 16, player_1.board.cells.count
+    assert_instance_of Player, player_1
+  end
+
+  def test_player_board_cells_populate
+    skip
+    player_1 = Player.new
+    cruiser = Ship.new("Cruiser", 3)
+    player_1.board.place(cruiser, ["A1", "A2", "A3"])
+    assert_equal "Cruiser" , player_1.board.cells["A3"].ship.name
+    assert_equal "Cruiser" , player_1.board.cells["A2"].ship.name
+    assert_equal "Cruiser" , player_1.board.cells["A1"].ship.name
+  end
+
+  def test_player_can_place_ships
+    player_1 = Player.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+    player_1.place_player_ship_three(cruiser, ["A1", "A2", "A3"])
+    player_1.place_player_ship_two(submarine, ["B2", "B3"])
+    binding.pry
+    assert_equal "Cruiser" , player_1.board.cells["A3"].ship.name
+    assert_equal "Cruiser" , player_1.board.cells["A2"].ship.name
+    assert_equal "Cruiser" , player_1.board.cells["A1"].ship.name
+    assert_equal "Submarine", player_1.board.cells["B2"].ship.name
+    assert_equal "Submarine", player_1.board.cells["B3"].ship.name
+  end
+
+
+
+end
